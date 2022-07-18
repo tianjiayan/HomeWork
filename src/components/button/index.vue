8d0fa1d68f865803925559674d544b8b3e02b99d
Tian <11132373+tianjiayan@user.noreply.gitee.com>
2022-07-18 20:49:55 +0800

    feat: button组件封装 - type属性生成主题按钮

--- src/components/button/index.vue
+++ src/components/button/index.vue
@@ -1,17 +1,6 @@
 <template>
-  <button
-    @click="change"
-    :disabled="disabled || loading"
-    class="yang-button"
-    :style="[minWidthCss]"
-    :class="[theme, isRound, isBorder, isSize, blockCss]"
-  >
-    <span>
-      <i v-if="loading" class="iconfont icon-prefix icon-loading"></i>
-      <i v-if="prefix" class="iconfont icon-prefix" :class="iconPrefix"></i>
-      <slot></slot>
-      <i v-if="suffix" class="iconfont icon-suffix" :class="iconSuffix"></i>
-    </span>
+  <button class="yang-button" :class="[theme]">
+    <slot></slot>
   </button>
 </template>
 
@@ -22,64 +11,47 @@ export default {
     type: {
       type: String,
       default: ''
-    },
-    minWidth: {
-      type: String,
-      default: '95px'
-    },
-    size: {
-      type: String,
-      default: ''
-    },
-    prefix: {
-      type: String,
-      default: ''
-    },
-    suffix: {
-      type: String,
-      default: ''
-    },
-    round: Boolean,
-    border: Boolean,
-    disabled: Boolean,
-    block: Boolean,
-    loading: Boolean
+    }
   },
   computed: {
     theme() {
       return this.type ? `yang-button-${this.type}` : ''
-    },
-    isRound() {
-      return this.round ? 'is-round' : ''
-    },
-    isBorder() {
-      return this.border ? 'is-border' : ''
-    },
-    isSize() {
-      return this.size ? `yang-button-${this.size}` : ''
-    },
-    minWidthCss() {
-      if (!this.minWidth) return ''
-      return { 'min-width': this.minWidth }
-    },
-    iconPrefix() {
-      return this.prefix ? `icon-${this.prefix}` : ''
-    },
-    iconSuffix() {
-      return this.suffix ? `icon-${this.suffix}` : ''
-    },
-    blockCss() {
-      return this.block ? 'yang-button-block' : ''
-    }
-  },
-  methods: {
-    change() {
-      this.$emit('click')
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-@import './button.scss';
+.yang-button {
+  border-width: 1px;
+  border-style: solid;
+  border-color: #dcdfe6;
+  height: 40px;
+  padding: 0 20px;
+  background-color: #fff;
+  border-radius: 4px;
+  font-size: 14px;
+  color: #606266;
+  cursor: pointer;
+}
+.yang-button-primary {
+  background-color: #409eff;
+  border-color: #409eff;
+  color: #fff;
+}
+.yang-button-success {
+  background-color: #00d100;
+  border-color: #00d100;
+  color: #fff;
+}
+.yang-button-danger {
+  background-color: #e6a23c;
+  border-color: #e6a23c;
+  color: #fff;
+}
+.yang-button-warning {
+  background-color: #f56c6c;
+  border-color: #f56c6c;
+  color: #fff;
+}
 </style>
---
+++ src/views/home/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <yangButton>默认按钮</yangButton>
+    <yangButton type="primary">成功按钮</yangButton>
+    <yangButton type="danger">危险按钮</yangButton>
+    <yangButton type="warning">警告按钮</yangButton>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components: {
+    yangButton: () => import('../components/button/index.vue')
+  }
+}
+</script>
