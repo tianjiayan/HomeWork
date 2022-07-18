007256c0c0d2bb4adc90e5cef7ac2f01beb39b61
Tian <11132373+tianjiayan@user.noreply.gitee.com>
2022-07-18 21:18:45 +0800

    size

--- src/components/button/index.vue
+++ src/components/button/index.vue
@@ -2,7 +2,7 @@
   <button
     :disabled="disabled"
     class="yang-button"
-    :class="[theme, isRound, isBorder]"
+    :class="[theme, isRound, isBorder, isSize]"
   >
     <slot></slot>
   </button>
@@ -13,6 +13,10 @@ export default {
   name: "index",
   props: {
     type: {
+      type: String,
+      default: "",
+    },
+    size: {
       type: String,
       default: "",
     },
@@ -30,6 +34,9 @@ export default {
     isBorder() {
       return this.border ? "is-border" : "";
     },
+    isSize() {
+      return this.size ? `yang-button-${this.size}` : "";
+    },
   },
 };
 </script>
@@ -46,30 +53,69 @@ export default {
   font-size: 14px;
   color: #606266;
   cursor: pointer;
+
+  + .yang-button {
+    margin-left: 14px;
+  }
+}
+.yang-button-medium {
+  height: 38px;
+}
+.yang-button-small {
+  padding: 0 15px;
+  height: 32px;
+  font-size: 12px;
+}
+.yang-button-mini {
+  padding: 0 15px;
+  height: 28px;
+  font-size: 12px;
+}
+.yang-button[disabled] {
+  cursor: not-allowed;
+  opacity: 0.5;
 }
 .yang-button-primary {
   background-color: #409eff;
   border-color: #409eff;
   color: #fff;
+  &.is-border {
+    background-color: transparent;
+    color: #409eff;
+  }
 }
 .yang-button-success {
   background-color: #00d100;
   border-color: #00d100;
   color: #fff;
+  &.is-border {
+    background-color: transparent;
+    color: #00d100;
+  }
 }
 .yang-button-danger {
   background-color: #e6a23c;
   border-color: #e6a23c;
   color: #fff;
+  &.is-border {
+    background-color: transparent;
+    color: #e6a23c;
+  }
 }
 .yang-button-warning {
   background-color: #f56c6c;
   border-color: #f56c6c;
   color: #fff;
+  &.is-border {
+    background-color: transparent;
+    color: #f56c6c;
+  }
 }
-// css
-.yang-button[disabled] {
-  cursor: not-allowed;
-  opacity: 0.5;
+
+/**
+ * 圆角
+ */
+.is-round {
+  border-radius: 100px;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="home">
     <yangButton>默认按钮</yangButton>
-    <yangButton type="primary" round disabled>成功按钮</yangButton>
-    <yangButton type="danger" round border disabled>危险按钮</yangButton>
-    <yangButton type="warning" border>警告按钮</yangButton>
+    <yangButton size="medium" type="primary" round disabled>成功按钮</yangButton>
+    <yangButton size="small" type="danger" round border disabled>危险按钮</yangButton>
+    <yangButton size="mini" type="warning" border>警告按钮</yangButton>
   </div>
 </template>
 
